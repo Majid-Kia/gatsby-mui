8f80fee7e117310a613ae43b520d7031ac068e49
shiva-mlk <shivamaleki112@yahoo.com>
2023-02-20 05:21:07 -0800

    just hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,14 +15,14 @@ import PagesDefaultLayout from "../components/layouts/PagesDefaultLayout";
 // import TestimonialSection from "../sections/testimonial-section/TestimonialSection";
 // import PricingCtaSection from "../sections/pricing-cta-section/PricingCtaSection";
 // import PricingCtaTempData from "../sections/pricing-cta-section/data/pricingCtaDescription.data";
-// import HomeHeroSection from "../sections/hero-sections/home-hero-section/HomeHeroSection";
+import HomeHeroSection from "../sections/hero-sections/home-hero-section/HomeHeroSection";
 
 export default function Index() {
   const pt = { xl: 18.75, lg: 18.75, md: 15, sm: 12.5, xs: 12.5 };
   return (
     <PagesDefaultLayout>
-      {/* <HomeHeroSection mt={{ lg: 0, md: 12, sm: 8, xs: 3 }} />
-      <PricingTableSection
+      <HomeHeroSection mt={{ lg: 0, md: 12, sm: 8, xs: 3 }} />
+      {/* <PricingTableSection
         pt={pt}
         pricingDataKey={"WINDOWS_VS_LINUX"}
         pageTitle={"Cloud VPS"}
@@ -39,7 +39,7 @@ export default function Index() {
         data={mostPopularServicesData}
       />
       <TestimonialSection pt={pt} testimonialIds={[1, 2, 4, 5]} />
-      <PricingCtaSection pt={pt} data={PricingCtaTempData} /> */}
+      <PricingCtaSection pt={pt} data={PricingCtaTempData} />  */}
     </PagesDefaultLayout>
   );
 }
