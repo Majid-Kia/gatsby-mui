6ef9d54e78dad57b4d21806708b232ec1a768f59
shiva-mlk <shivamaleki112@yahoo.com>
2023-02-21 03:03:04 -0800

    testimonial

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,10 +9,10 @@ import React from "react";
 // import WhyChooseSection from "../sections/why-choose-section/WhyChooseSection";
 // import MapSection from "../sections/map-section/MapSection";
 import PagesDefaultLayout from "../components/layouts/PagesDefaultLayout";
-import PricingTableSection from "../sections/pricing-table-section/PricingTableSection";
+// import PricingTableSection from "../sections/pricing-table-section/PricingTableSection";
 // import mostPopularServicesData from "../sections/most-popular-services-sections/data/mostPopularServices.data";
 // import MostPopularServicesSection from "../sections/most-popular-services-sections/MostPopularServicesSection";
-// import TestimonialSection from "../sections/testimonial-section/TestimonialSection";
+import TestimonialSection from "../sections/testimonial-section/TestimonialSection";
 // import PricingCtaSection from "../sections/pricing-cta-section/PricingCtaSection";
 // import PricingCtaTempData from "../sections/pricing-cta-section/data/pricingCtaDescription.data";
 // import HomeHeroSection from "../sections/hero-sections/home-hero-section/HomeHeroSection";
@@ -22,11 +22,11 @@ export default function Index() {
   return (
     <PagesDefaultLayout>
       {/* <HomeHeroSection mt={{ lg: 0, md: 12, sm: 8, xs: 3 }} /> */}
-      <PricingTableSection
+      {/* <PricingTableSection
         pt={pt}
         pricingDataKey={"WINDOWS_VS_LINUX"}
         pageTitle={"Cloud VPS"}
-      />
+      /> */}
 
       {/* <FullWidthContainer pt={pt}>
         <WhyChooseSection />
@@ -38,7 +38,7 @@ export default function Index() {
         title="Our Most Popular Services"
         data={mostPopularServicesData}
       /> */}
-      {/* <TestimonialSection pt={pt} testimonialIds={[1, 2, 4, 5]} /> */}
+      <TestimonialSection pt={pt} testimonialIds={[1, 2, 4, 5]} />
       {/* <PricingCtaSection pt={pt} data={PricingCtaTempData} /> */}
     </PagesDefaultLayout>
   );
