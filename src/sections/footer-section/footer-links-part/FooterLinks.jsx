828fac5384d253e6fdfa2a5ee45acf4195448e49
shiva-mlk <shivamaleki112@yahoo.com>
2023-02-21 05:36:54 -0800

    delete poly

--- src/sections/footer-section/footer-links-part/FooterLinks.jsx
+++ src/sections/footer-section/footer-links-part/FooterLinks.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
-import PolymorphicOnResizeContainer from '../../../components/container/PolymorphicOnResizeContainer';
-import FooterLargeMode from './footer-links-large-mode/FooterLargeMode';
 import FooterSmallMode from './FooterSmallMode';
 
 const FooterLinks = () => {
   return (
     <Grid container>
       <Grid item lg={12} md={12} sm={12} xs={12}>
-        <PolymorphicOnResizeContainer
-          largeModeComponent={<FooterLargeMode />}
-          smallModeComponent={<FooterSmallMode />}
-        />
+        <FooterSmallMode />
       </Grid>
     </Grid>
   );
