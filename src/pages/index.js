99f9ddda16e560ddbad3f348379f938715285515
shiva-mlk <shivamaleki112@yahoo.com>
2023-02-21 03:39:24 -0800

    hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,18 +10,18 @@ import React from "react";
 // import MapSection from "../sections/map-section/MapSection";
 import PagesDefaultLayout from "../components/layouts/PagesDefaultLayout";
 // import PricingTableSection from "../sections/pricing-table-section/PricingTableSection";
-import mostPopularServicesData from "../sections/most-popular-services-sections/data/mostPopularServices.data";
-import MostPopularServicesSection from "../sections/most-popular-services-sections/MostPopularServicesSection";
+// import mostPopularServicesData from "../sections/most-popular-services-sections/data/mostPopularServices.data";
+// import MostPopularServicesSection from "../sections/most-popular-services-sections/MostPopularServicesSection";
 // import TestimonialSection from "../sections/testimonial-section/TestimonialSection";
 // import PricingCtaSection from "../sections/pricing-cta-section/PricingCtaSection";
 // import PricingCtaTempData from "../sections/pricing-cta-section/data/pricingCtaDescription.data";
-// import HomeHeroSection from "../sections/hero-sections/home-hero-section/HomeHeroSection";
+import HomeHeroSection from "../sections/hero-sections/home-hero-section/HomeHeroSection";
 
 export default function Index() {
   const pt = { xl: 18.75, lg: 18.75, md: 15, sm: 12.5, xs: 12.5 };
   return (
     <PagesDefaultLayout>
-      {/* <HomeHeroSection mt={{ lg: 0, md: 12, sm: 8, xs: 3 }} /> */}
+      <HomeHeroSection mt={{ lg: 0, md: 12, sm: 8, xs: 3 }} />
       {/* <PricingTableSection
         pt={pt}
         pricingDataKey={"WINDOWS_VS_LINUX"}
@@ -32,12 +32,12 @@ export default function Index() {
         <WhyChooseSection />
       </FullWidthContainer> */}
       {/* <MapSection pt={pt} /> */}
-
+      {/* 
       <MostPopularServicesSection
         pt={pt}
         title="Our Most Popular Services"
         data={mostPopularServicesData}
-      />
+      /> */}
       {/* <TestimonialSection pt={pt} testimonialIds={[1, 2, 4, 5]} /> */}
       {/* <PricingCtaSection pt={pt} data={PricingCtaTempData} /> */}
     </PagesDefaultLayout>
