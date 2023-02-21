66d7d632209a07af7bc28b5a8e82561edcf2fe8a
shiva-mlk <shivamaleki112@yahoo.com>
2023-02-21 05:12:22 -0800

    most popular

--- src/components/tab-accordion/TabAccordion.jsx
+++ src/components/tab-accordion/TabAccordion.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
-import PolymorphicOnResizeContainer from '../container/PolymorphicOnResizeContainer';
-import TabAccordionLargeMode from './TabAccordionLargeMode';
+// import PolymorphicOnResizeContainer from '../container/PolymorphicOnResizeContainer';
+// import TabAccordionLargeMode from './TabAccordionLargeMode';
 import PropTypes from 'prop-types';
 import TabAccordionSmallMode from './TabAccordionSmallMode';
 
