915e89c7ae0fb710495f9b5dc93bd3b3b92f9e00
shiva-mlk <shivamaleki112@yahoo.com>
2023-02-22 00:09:32 -0800

    minimize js in footer

--- src/sections/footer-section/data/FooterLinks.data.js
+++ src/sections/footer-section/data/FooterLinks.data.js
@@ -96,38 +96,7 @@ module.exports = [
             title: 'OpenSUSE VPS',
             link: '/opensuse-vps/',
           },
-          {
-            title: 'Slackware VPS',
-            link: '/slackware-vps/',
-          },
-          {
-            title: 'Scientific Linux VPS',
-            link: '/scientific-linux-vps/',
-          },
-          {
-            title: 'Fedora VPS',
-            link: '/fedora-vps/',
-          },
-          {
-            title: 'Arch Linux VPS',
-            link: '/arch-linux-vps/',
-          },
-          {
-            title: 'Linux Mint VPS',
-            link: '/linux-mint-vps/',
-          },
-          {
-            title: 'CloudLinux VPS',
-            link: '/cloudlinux-vps/',
-          },
-          {
-            title: 'AlmaLinux VPS',
-            link: '/almalinux-vps/',
-          },
-          {
-            title: 'Rocky Linux VPS',
-            link: '/rocky-linux-vps/',
-          },
+
         ],
       },
     ],
@@ -199,201 +168,6 @@ module.exports = [
             title: 'PayPal VPS',
             link: '/paypal-vps/',
           },
-          {
-            title: 'DDos Protected VPS',
-            link: '/ddos-protected-vps/',
-          },
-          {
-            title: 'SEO VPS',
-            link: '/seo-vps/',
-          },
-          {
-            title: 'MikroTik VPS',
-            link: '/mikrotik-vps/',
-          },
-          {
-            title: 'Android VPS',
-            link: '/bluestacks-vps/',
-          },
-          {
-            title: 'Python VPS',
-            link: '/python-vps-hosting/',
-          },
-          {
-            title: 'Forex RDP VPS',
-            link: '/forex-rdp/',
-          },
-          {
-            title: 'Botting RDP',
-            link: '/botting-rdp/',
-          },
-          {
-            title: 'Storage VPS',
-            link: '/storage-vps/',
-          },
-          {
-            title: 'Craft CMS Hosting VPS',
-            link: '/craft-cms-hosting/',
-          },
-          {
-            title: 'WordPress VPS',
-            link: '/wordpress-vps/',
-          },
-          {
-            title: 'Magento VPS',
-            link: '/magento-vps/',
-          },
-          {
-            title: 'Ripple VPS',
-            link: '/ripple-vps/',
-          },
-          {
-            title: 'Hyper-V VPS',
-            link: '/hyper-v-vps/',
-          },
-          {
-            title: 'Cloud VPS',
-            link: '/cloud/',
-          },
-        ],
-      },
-    ],
-  },
-  {
-    title: 'Locations',
-    items: [
-      {
-        title: 'USA',
-        isCollapse: true,
-        items: [
-          {
-            title: 'USA VPS',
-            link: '/usa-vps/',
-          },
-          {
-            title: 'Seattle VPS',
-            link: '/seattle-vps/',
-          },
-          {
-            title: 'Miami VPS',
-            link: '/miami-vps/',
-          },
-          {
-            title: 'Phoenix VPS',
-            link: '/phoenix-vps/',
-          },
-          {
-            title: 'Chicago VPS',
-            link: '/chicago-vps/',
-          },
-          {
-            title: 'Buffalo VPS',
-            link: '/buffalo-vps/',
-          },
-          {
-            title: 'Washington VPS',
-            link: '/washington-dc-vps/',
-          },
-          {
-            title: 'Pittsburgh VPS',
-            link: '/pittsburgh-vps/',
-          },
-          {
-            title: 'Los Angeles VPS',
-            link: '/los-angeles-vps/',
-          },
-          {
-            title: 'Residential IP VPS',
-            link: '/residential-ip-vps/',
-          },
-          {
-            title: 'San Francisco VPS',
-            link: '/san-francisco-vps/',
-          },
-          {
-            title: 'Dallas VPS',
-            link: '/dallas-vps/',
-          },
-          {
-            title: 'New York VPS',
-            link: '/new-york-vps/',
-          },
-          {
-            title: 'Silicon Valley VPS',
-            link: '/silicon-valley-vps/',
-          },
-          {
-            title: 'Atlanta VPS',
-            link: '/atlanta-vps/',
-          },
-          {
-            title: 'Florida VPS',
-            link: '/florida-vps/',
-          },
-          {
-            title: 'Canada VPS',
-            link: '/canada-vps/',
-          },
-          {
-            title: 'USA RDP',
-            link: '/usa-rdp/',
-          },
-        ],
-      },
-      {
-        title: 'Asia',
-        isCollapse: true,
-        items: [
-          {
-            title: 'Asia VPS',
-            link: '/asia-vps/',
-          },
-          {
-            title: 'Singapore VPS',
-            link: '/singapore-vps/',
-          },
-        ],
-      },
-      {
-        title: 'Europe',
-        isCollapse: true,
-        items: [
-          {
-            title: 'Europe VPS',
-            link: '/europe-vps/',
-          },
-          {
-            title: 'UK VPS',
-            link: '/uk-vps/',
-          },
-          {
-            title: 'London VPS',
-            link: '/london-vps/',
-          },
-          {
-            title: 'France VPS',
-            link: '/france-vps/',
-          },
-          {
-            title: 'Roubaix VPS',
-            link: '/roubaix-vps/',
-          },
-          {
-            title: 'Netherlands VPS',
-            link: '/netherlands-vps/',
-          },
-          {
-            title: 'Amsterdam VPS',
-            link: '/amsterdam-vps/',
-          },
-          {
-            title: 'Germany VPS',
-            link: '/germany-vps/',
-          },
-          {
-            title: 'Frankfurt VPS',
-            link: '/frankfurt-vps/',
-          },
         ],
       },
     ],
@@ -409,18 +183,7 @@ module.exports = [
         title: 'Best RDP Provider',
         link: '/best-rdp-provider/',
       },
-      {
-        title: 'Best Linux VPS',
-        link: '/best-linux-vps/',
-      },
-      {
-        title: 'How to order a VPS',
-        link: '/blog/how-to-order-a-vps/',
-      },
-      {
-        title: 'How to Pick the Best VPS',
-        link: '/best-vps/',
-      },
+
     ],
   },
   {
@@ -442,26 +205,7 @@ module.exports = [
         title: 'FAQ',
         link: '/faq/',
       },
-      {
-        title: 'Blog',
-        link: '/blog/',
-      },
-      {
-        title: 'Customer Reviews',
-        link: '/vps-reviews/',
-      },
-      {
-        title: 'Money-back Guarantee',
-        link: '/money-back-guarantee/',
-      },
-      {
-        title: 'Privacy Policy',
-        link: '/privacy-policy/',
-      },
-      {
-        title: 'Terms of Service',
-        link: '/terms-of-service/',
-      },
+
     ],
   },
 ];
